260fc6413bb2dd3b1f05dbfd50c9fcf4099baa59
robuno <42478761+robuno@users.noreply.github.com>
2020-12-31 21:56:45 +0300

    initial-comm

---
+++ src/com/teksen/Cipher.java
@@ -0,0 +1,103 @@
+package com.teksen;
+import java.io.*;
+/**
+ * <h1> Encrypts and Decrypt the Text File </h1>
+ * The Cipher program implements that
+ *
+ *
+ * @author Unat Teksen
+ * 20181701048
+ * Computer Engineering
+ * @version 1.0
+ * @since 19-12-2020
+ */
+public class Cipher {
+    private static String newText= ""; //
+
+    /**
+     * This method shift the characters in old text if they are
+     * characters of alphabet. Checks the character is upper or lower case
+     * and change it with one of the next characters according to the
+     * given key.
+     *
+     * @param oldText is the text of the txt file
+     * @param key ist he number of shift
+     * return nothing
+     */
+    public static void encrypt(String oldText, int key){
+        // manipulate all characters in the text of txt file
+        for(int i=0; i < oldText.length();i++) {
+            char character = oldText.charAt(i); // character in txt file's text in index i
+
+            if(character >= 'a' && character <= 'z') { // if character is part of lower case alphabet
+                character = (char) (character + key); // assign the character with shifted the character with given key
+
+                if(character > 'z') { // if character is not part of alphabet
+                    character = (char) (character+'a'-'z'-1); // assign character to the existed character
+                }
+                newText = newText + character; // add changed character to the new text
+            }
+
+            else if(character >= 'A' && character <= 'Z') { // if character is part of upper case alphabet
+                character = (char) (character + key); // assign the character with shifted the character with given key
+
+                if(character > 'Z') { // if character is not part of alphabet
+                    character = (char) (character+'A'-'Z'-1); // assign character to the existed character
+                }
+                newText = newText + character; // add changed character to the new text
+            } else {
+                newText = newText + character; // add changed character to the new text
+            }
+        }
+    }
+    /**
+     * This method shift the characters in old text if they are
+     * characters of alphabet. Checks the character is upper or lower case
+     * and change it with one of the previous characters according to the
+     * given key.
+     *
+     * @param oldText is the text of the txt file
+     * @param key ist he number of shift
+     * return nothing
+     */
+    public static void decrypt(String oldText, int key) {
+        // manipulate all characters in the text of txt file
+        for(int i=0; i < oldText.length();i++) {
+            char oldCharacter = oldText.charAt(i); // character in txt file's text in index i
+
+            if(oldCharacter >= 'a' && oldCharacter <= 'z') { // if character is part of lower case alphabet
+                oldCharacter = (char) (oldCharacter - key); // assign the character with shifted the character with given key
+
+                if(oldCharacter < 'a') { // if character is not part of alphabet
+                    oldCharacter = (char) (oldCharacter-'a'+'z'+1); // assign character to the existed character
+                }
+                newText = newText + oldCharacter; // add changed character to the new text
+            }
+
+            else if(oldCharacter >= 'A' && oldCharacter <= 'Z') { // if character is part of upper case alphabet
+                oldCharacter = (char) (oldCharacter - key); // assign the character with shifted the character with given key
+
+                if (oldCharacter < 'A') { // if character is not part of alphabet
+                    oldCharacter = (char) (oldCharacter-'A'+'Z'+1); // assign character to the existed character
+                }
+                newText = newText + oldCharacter; // add changed character to the new text
+            } else {
+                newText = newText + oldCharacter; // add changed character to the new text
+            }
+        }
+
+    }
+
+    /**
+     * This method writes the newText which is created by
+     * encrypt/decrypt functions on the given txt.
+     * @param fileName is the name of the file which the writer function writes new text on
+     * @throws IOException
+     * return nothing
+     */
+    public static void writer(String fileName) throws IOException {
+        FileWriter myWriter = new FileWriter(fileName); // create an object with the txt file to write on it
+        myWriter.write(newText); // write new text on txt file
+        myWriter.close(); // close the txt file
+    }
+}
---
+++ src/com/teksen/CipherTest.java
@@ -0,0 +1,68 @@
+package com.teksen;
+import java.io.*;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+/**
+ * <h1> Encrypts and Decrypt the Text File </h1>
+ * The CipherTest program implements that
+ * takes the user's choice to encrypt/decrypt the txt file
+ * which is provided by user and takes the key to shift
+ * the order of alphabet.
+ *
+ * @author Unat Teksen
+ * 20181701048
+ * Computer Engineering
+ * @version 1.0
+ * @since 19-12-2020
+ */
+
+public class CipherTest {
+    /**
+     * This is the main method which makes use of CipherTest method.
+     * Gets input from user in String and input form , creates object of Cipher class
+     * and read-convert the txt file to the String. Call the related functions to
+     * manipulate text in the txt file.
+     *
+     * @param args
+     * return nothing
+     */
+    public static void main(String[] args) throws IOException {
+        // create a Scanner to obtain input from command window.
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("""
+                Greetings, what would you like to do?
+                1 - Encrypt the file
+                2 - Decrypt the file"""); // prompt
+        int answer = input.nextInt(); // choice of user to determine encrypt/decrypt
+
+        System.out.println("Please provide the file name."); // prompt
+        String fileName = input.next(); // name of txt file
+
+        String oldText = ""; // blank String which will be text in file
+        // create a Scanner to obtain the txt file
+        Scanner inputPath = new Scanner(Paths.get(fileName));
+        while (inputPath.hasNext()) { //  read the file if there is a character
+            oldText = oldText + (inputPath.next()+" "); // add these characters to create full String text
+        }
+
+        if (answer == 1){ // encryption
+            System.out.println("What will be your key?"); // prompt
+            int key = input.nextInt(); // number of shift
+
+            Cipher.encrypt(oldText, key); // call encryption function which belong to Cipher class
+            Cipher.writer(fileName); // call writing function which belong to Cipher class
+            System.out.print("Done! Input encrypted and saved! "); // prompt
+        }
+
+        if (answer == 2){
+            System.out.println("What was the key you used to encrypt this text?"); // prompt
+            int key = input.nextInt(); // number of shift
+
+            Cipher.decrypt(oldText, key); // call decryption function which belong to Cipher class
+            Cipher.writer(fileName); // call writing function which belong to Cipher class
+            System.out.print("Done! Input decrypted and saved! "); // prompt
+        }
+    }
+}
